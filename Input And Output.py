73cce487377ac90d552ba13afde0d66ce0b6c748
Pawan Kumar <pwnishp.tech@gmail.com>
2026-03-27 21:58:29 +0545

    Taken Two Numbers As Input And Printed Their Sum

--- Input And Output.py
+++ Input And Output.py
@@ -1,3 +1,9 @@
 # 21. Take user input for name and print greeting
 userName = str(input("Enter Your Name :- "))
 print("Welcome",userName)
+
+
+# 22. Take two numbers as input and print their sum
+num1 = int(input("Enter First Number:-"))
+num2 = int(input("Enter Second Number :-"))
+print("The Sum Of Two Numbers Is :",num1+num2)
