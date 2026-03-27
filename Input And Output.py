342f33447f05a769559c11b9e76a9b57d5268538
Pawan Kumar <pwnishp.tech@gmail.com>
2026-03-27 22:01:21 +0545

    Taken Integer As Input And Checked Weather It Is Negative Or Positive

--- Input And Output.py
+++ Input And Output.py
@@ -7,3 +7,11 @@ print("Welcome",userName)
 num1 = int(input("Enter First Number:-"))
 num2 = int(input("Enter Second Number :-"))
 print("The Sum Of Two Numbers Is :",num1+num2)
+
+
+# 23. Take integer input and check if it's positive
+inputNum = int(input("Enter The Number :-"))
+if inputNum > 0 :
+    print("Positive")
+else:
+    print("Negative")
