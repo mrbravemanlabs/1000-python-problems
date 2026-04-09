8e02853da29038a6b1da35a8fcc55cf6c25fddd4
Pawan Kumar <pwnishp.tech@gmail.com>
2026-04-09 22:45:11 +0545

    Taken Multiple Inputs in one line

--- Input And Output.py
+++ Input And Output.py
@@ -1,17 +1,22 @@
 # 21. Take user input for name and print greeting
-userName = str(input("Enter Your Name :- "))
-print("Welcome",userName)
+# userName = str(input("Enter Your Name :- "))
+# print("Welcome",userName)
 
 
 # 22. Take two numbers as input and print their sum
-num1 = int(input("Enter First Number:-"))
-num2 = int(input("Enter Second Number :-"))
-print("The Sum Of Two Numbers Is :",num1+num2)
+# num1 = int(input("Enter First Number:-"))
+# num2 = int(input("Enter Second Number :-"))
+# print("The Sum Of Two Numbers Is :",num1+num2)
 
 
 # 23. Take integer input and check if it's positive
-inputNum = int(input("Enter The Number :-"))
-if inputNum > 0 :
-    print("Positive")
-else:
-    print("Negative")
+# inputNum = int(input("Enter The Number :-"))
+# if inputNum > 0 :
+#     print("Positive")
+# else:
+#     print("Negative")
+    
+    
+# 24. Take multiple inputs in one line
+x,y,z = input("Enter Values :-").split()
+print(x,y,z)
